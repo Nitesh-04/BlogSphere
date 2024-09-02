3074331e4e69547a49cd4dcdcd9e95d5eb2ddf4c
nitesh kakkar <kakkar.nitesh04@gmail.com>
2024-09-02 19:52:46 +0530

    we keep trying

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/blogs", "/blogs/(.*)","/api,/api/(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/blogs", "/blogs/(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
